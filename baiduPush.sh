91ce2e0a180f9d923d4f73a6d5360bbb76132f84
AlanRain <alanpeng@live.com>
2020-05-14 20:05:53 +0800

    - 生成urls.txt便于测试

--- baiduPush.sh
+++ baiduPush.sh
@@ -5,4 +5,4 @@ set -e
 # 百度链接推送
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://pengruifei.github.io&token=xdqIllqVgswdi3qm"
 
-rm -rf urls.txt # 灭迹
+# rm -rf urls.txt # 灭迹
